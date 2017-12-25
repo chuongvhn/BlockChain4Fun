71a3569c0e8685fabf84e65a55a81546968c2a8f
Calvin Vo <calvin.chuong.vo@gmail.com>
2017-12-25 21:48:55 +0700

    add option for cleaning

--- run_build.bat
+++ run_build.bat
@@ -1,8 +1,34 @@
 ::
 ::automatic build
 
+ECHO OFF
+SET opt_1=%1
+SET opt_2=%2 
+SHIFT & SHIFT
+
+:loop
+IF NOT "%1"=="" (
+    IF "%1"=="--clean" (
+        SET do_clean=1
+        SHIFT
+    )
+    IF "%1"=="-to_be_used_option" (
+        SET to_be_used=%2
+        SHIFT
+    )
+    SHIFT
+    GOTO :loop
+)
+
+ECHO opt_1 = %opt_1%
+ECHO opt_2 = %opt_2%
+
+IF %do_clean%==1 goto CLEAN:
 
 cd build
 cmake ../ -DCMAKE_INSTALL_PREFIX=./
 cmake --build ./ --config release --target install
 cd ..
+
+CLEAN:
+ECHO cleaning...
